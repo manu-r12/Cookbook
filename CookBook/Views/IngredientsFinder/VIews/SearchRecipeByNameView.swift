087401cb2910cd3998c01e359bd684629fc131c0
Manu R <manurajbhar12@gmail.com>
2024-08-05 21:30:23 +0530

    made some changes

--- CookBook/Views/IngredientsFinder/VIews/SearchRecipeByNameView.swift
+++ CookBook/Views/IngredientsFinder/VIews/SearchRecipeByNameView.swift
@@ -18,61 +18,48 @@ struct SearchRecipeByNameView: View {
     @State var selectedMethod: SearchMethod = .SearchByTitle
     @ObservedObject var viewModel: IngredientsFInderOptionsViewModel
     
-    func getSearchResult() async {
-        do {
-            print("Getting the data")
 
-            let res = try await   viewModel
-                .fetchRecipesInfo(
-                    query: searchTextInput,
-                    numberOfRes: 5,
-                    searchMethod: .SearchByTitle
-                )
-            print("Got the data => \(res)")
-        }catch{
-            print("Error in Fetching", error.localizedDescription)
-        }
-        
-    }
-    
     var body: some View {
-        VStack(spacing: 20){
-            VStack(spacing: 25){
-                VStack {
-                    Text("Select the Search Method")
-                        .font(.custom("Poppins-Regular", size: 21))
-                        .onTapGesture {
-                            Task{
-                                await getSearchResult()
-                            }
-                        }
-                }
-                
-                HStack{
-                    ForEach(SearchMethod.allCases, id: \.self){ method in
-                        Button {
-                            withAnimation(.smooth) {
-                                selectedMethod = method
-                            }
-                            
-                            
-                        } label: {
-                            Text(method.rawValue)
-                                .padding()
-                                .background(
-                                    selectedMethod == method ? .akGreen : .akBg
-                                )
-                                .clipShape(RoundedRectangle(cornerRadius: 18))
-                                .kerning(0.86)
-                        }
-                        .tint(.white)
-                        
+        ScrollView{
+            VStack(spacing: 20){
+                VStack(spacing: 25){
+                    VStack {
+                        Text("Select the Search Method")
+                            .font(.custom("Poppins-Regular", size: 21))
                     }
+    
+                    HStack{
+                        ForEach(SearchMethod.allCases, id: \.self){ method in
+                            Button {
+                                withAnimation(.smooth) {
+                                    selectedMethod = method
+                                }
+    
+    
+                            } label: {
+                                Text(method.rawValue)
+                                    .padding()
+                                    .background(
+                                        selectedMethod == method ? .akGreen : .akBg
+                                    )
+                                    .clipShape(RoundedRectangle(cornerRadius: 18))
+                                    .kerning(0.86)
+                            }
+                            .tint(.white)
+    
+                        }
+                    }
+    
                 }
+                VStack{
+                    SearchBar(searchText: $searchTextInput)
+                        .onSubmit {
+                            print("\(searchTextInput)")
+                        }
+                }
+           
                 
             }
-            
-            SearchBar(searchText: $searchTextInput)
         }
     }
 }
